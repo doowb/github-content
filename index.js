63f21dfdeaf6f7812e798b037791f46fd770d44b
Brian Woodward <brian.woodward@gmail.com>
2015-11-12 12:13:16 -0500

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * github-content <https://github.com/doowb/github-content>
+ *
+ * Copyright (c) 2015, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
