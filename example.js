25d84e244cda207d2ef5f302976a76082cc877bd
doowb <brian.woodward@gmail.com>
2017-09-11 10:37:04 -0400

    update example for refactored code

--- example.js
+++ example.js
@@ -1,17 +1,17 @@
 'use strict';
 
 var GithubContent = require('./');
-var gc = new GithubContent();
-
-gc.owner('doowb')
-  .repo('handlebars-helpers')
-  .branch('docs');
+var gc = new GithubContent({
+  owner: 'doowb',
+  repo: 'handlebars-helpers',
+  branch: 'docs'
+});
 
 gc.files(['scaffolds.json', 'package.json'], function(err, results) {
   if (err) return console.error(err);
   results.forEach(function(file) {
     console.log('----', file.path, '----');
-    console.log(file.content);
+    console.log(file.contents.toString());
     console.log();
   });
 });
