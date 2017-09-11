1a8324d59643336346341b90d6249da944995e2c
doowb <brian.woodward@gmail.com>
2017-09-11 10:37:20 -0400

    update tests for refactored code and to get code coverage

--- test.js
+++ test.js
@@ -12,124 +12,177 @@ var assert = require('assert');
 var sinon = require('sinon');
 var GithubContent = require('./');
 
-describe('github-content', function () {
-  it('should create an instance with default options', function () {
+describe('github-content', function() {
+  it('should create an instance with default options', function() {
     var gc = new GithubContent();
     assert(gc instanceof GithubContent);
     assert.equal(gc.options.json, false);
     assert.equal(gc.options.apiurl, 'https://raw.githubusercontent.com');
   });
 
-  it('should create an instance with default options without using new', function () {
+  it('should create an instance with default options without using new', function() {
     var gc = GithubContent();
     assert(gc instanceof GithubContent);
     assert.equal(gc.options.json, false);
     assert.equal(gc.options.apiurl, 'https://raw.githubusercontent.com');
   });
 
-  it('should create an instance with specified cache values', function () {
+  it('should create an instance with specified option values', function() {
     var gc = new GithubContent({
       owner: 'doowb',
       repo: 'github-content',
       branch: 'master'
     });
 
-    assert.equal(gc.cache.owner, 'doowb');
-    assert.equal(gc.cache.repo, 'github-content');
-    assert.equal(gc.cache.branch, 'master');
+    assert.equal(gc.options.owner, 'doowb');
+    assert.equal(gc.options.repo, 'github-content');
+    assert.equal(gc.options.branch, 'master');
   });
 
-  it('should set cache values through methods', function () {
-    var gc = new GithubContent();
-    gc.owner('doowb')
-      .repo('github-content')
-      .branch('master');
+  it('should throw an error when callback is not specified', function() {
+    var gc = new GithubContent({
+      owner: 'doowb',
+      repo: 'github-content',
+      branch: 'master'
+    });
 
-    assert.equal(gc.cache.owner, 'doowb');
-    assert.equal(gc.cache.repo, 'github-content');
-    assert.equal(gc.cache.branch, 'master');
-  });
-
-  it('should download a file', function (done) {
-    var gc = new GithubContent();
-    gc.owner('doowb')
-      .repo('github-content')
-      .branch('master');
-
-    gc.file('package.json', function(err, file) {
-      if (err) return done(err);
-      assert.equal(file.path, 'package.json');
-      done();
+    assert.throws(function() {
+      gc.file('package.json');
     });
   });
 
-  it('should not download a file', function (done) {
-    var gc = new GithubContent();
-    gc.owner('doowb')
-      .repo('github-content')
-      .branch('master');
+  it('should return an error when the repo is not specified', function(cb) {
+    var gc = new GithubContent({
+      owner: 'doowb',
+      branch: 'master'
+    });
+
+    gc.file('package.json', function(err, file) {
+      if (!err) {
+        cb(new Error('expected an error to be returned'));
+        return;
+      }
+      assert.equal(err.message, 'expected "options.repo" to be specified');
+      cb();
+    })
+  });
+
+  it('should download a file', function(cb) {
+    var gc = new GithubContent({
+      owner: 'doowb',
+      repo: 'github-content',
+      branch: 'master'
+    });
+
+    gc.file('package.json', function(err, file) {
+      if (err) {
+        cb(err);
+        return;
+      }
+      assert.equal(file.path, 'package.json');
+      cb();
+    });
+  });
+
+  it('should allow passing repo in :owner/:name format', function(cb) {
+    var gc = new GithubContent({repo: 'doowb/github-content'});
+
+    gc.file('package.json', function(err, file) {
+      if (err) {
+        cb(err);
+        return;
+      }
+      assert.equal(file.path, 'package.json');
+      cb();
+    });
+  });
+
+  it('should not download a file', function(cb) {
+    var gc = new GithubContent({
+      owner: 'doowb',
+      repo: 'github-content',
+      branch: 'master'
+    });
 
     // false is just to get code coverage
     gc.file('notfound.json', false, function(err, file) {
-      if (err) return done(err);
+      if (err) {
+        cb(err);
+        return;
+      }
       assert.equal(file.path, 'notfound.json');
-      assert.equal(file.content, '404: Not Found\n');
-      done();
+      assert.equal(file.contents, '404: Not Found\n');
+      cb();
     });
   });
 
-  it('should download a single file as an array of files', function (done) {
-    var gc = new GithubContent();
-    gc.owner('doowb')
-      .repo('github-content')
-      .branch('master');
+  it('should download a single file as an array of files', function(cb) {
+    var gc = new GithubContent({
+      owner: 'doowb',
+      repo: 'github-content',
+      branch: 'master'
+    });
 
     // false is just to get code coverage
     gc.files('package.json', false, function(err, files) {
-      if (err) return done(err);
+      if (err) {
+        cb(err);
+        return;
+      }
       assert.equal(files.length, 1);
       assert.equal(files[0].path, 'package.json');
-      done();
+      cb();
     });
   });
 
-  it('should download an array of files', function (done) {
-    var gc = new GithubContent();
-    gc.owner('doowb')
-      .repo('github-content')
-      .branch('master');
+  it('should download an array of files', function(cb) {
+    var gc = new GithubContent({
+      owner: 'doowb',
+      repo: 'github-content',
+      branch: 'master'
+    });
 
     gc.files(['package.json', 'index.js'], function(err, files) {
-      if (err) return done(err);
+      if (err) {
+        cb(err);
+        return;
+      }
       assert.equal(files.length, 2);
-      assert.equal(files[0].path, 'package.json');
-      assert.equal(files[1].path, 'index.js');
-      done();
+      files.forEach(function(file) {
+        assert(file.path === 'package.json' || file.path === 'index.js');
+      });
+      cb();
     });
   });
 
-  it('should not download null files', function (done) {
+  it('should not download null files', function(cb) {
     var gc = new GithubContent();
     gc.files(null, function(err, files) {
-      if (err) return done(err);
+      if (err) {
+        cb(err);
+        return;
+      }
       assert.equal(files.length, 0);
-      done();
+      cb();
     });
   });
 
-  it('should not download undefined files', function (done) {
+  it('should not download undefined files', function(cb) {
     var gc = new GithubContent();
     gc.files(undefined, function(err, files) {
-      if (err) return done(err);
+      if (err) {
+        cb(err);
+        return;
+      }
       assert.equal(files.length, 0);
-      done();
+      cb();
     });
   });
 
   describe('errors', function() {
     var gc = null;
     before(function(){
-      gc = new GithubContent();
+      gc = new GithubContent({owner: 'doowb', repo: 'github-content'});
       sinon
         .stub(gc, 'get')
         .yields(new Error('Fake Error'), null);
@@ -139,10 +192,10 @@ describe('github-content', function () {
       gc.get.restore();
     });
 
-    it('should error on invalid owner and repo', function (done) {
+    it('should error on invalid owner and repo', function(cb) {
       gc.files(['package.json'], function(err, files) {
         assert(err);
-        done();
+        cb();
       });
     });
   });
